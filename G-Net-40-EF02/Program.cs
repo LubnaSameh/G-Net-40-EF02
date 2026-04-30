3a1021f9d052ab7135a6ab35f6bc9d81fda22114
Lubna Sameh <lobnasameh777@gmail.com>
2026-04-30 08:40:11 +0300

    update solving

--- G-Net-40-EF02/Program.cs
+++ G-Net-40-EF02/Program.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Reflection.Emit;
 
 namespace G_Net_40_EF02
 {
@@ -50,18 +49,31 @@ namespace G_Net_40_EF02
     }
     #endregion
 
-    #region Domain Models (Fluent API for 1-to-1)
+    #region Owned Entity Type (Q02 Fix)
+    // Q02:
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+    }
+    #endregion
+
+    #region Domain Models (Fluent API & Others)
     public class Attendee
     {
         public int Id { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string PostalCode { get; set; }
+
+        // Address is now an Owned Type
+        public virtual Address Address { get; set; }
 
         public virtual Badge Badge { get; set; }
+
+        // Navigation property for Many-to-Many
+        public virtual ICollection<Registration> Registrations { get; set; } = new List<Registration>();
     }
 
     public class Badge
@@ -74,6 +86,39 @@ namespace G_Net_40_EF02
         public int AttendeeId { get; set; }
         public virtual Attendee Attendee { get; set; }
     }
+
+    // Q01 & Q04: 
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; } up
+        public int MaxAttendees { get; set; }
+
+        // Self-Referencing Relationship
+        public int? ParentEventId { get; set; }
+        public virtual Event ParentEvent { get; set; }
+        public virtual ICollection<Event> Sessions { get; set; } = new List<Event>();
+
+        // Navigation property for Many-to-Many
+        public virtual ICollection<Registration> Registrations { get; set; } = new List<Registration>();
+    }
+
+    // Q01: 
+    public class Registration
+    {
+        public int AttendeeId { get; set; }
+        public virtual Attendee Attendee { get; set; }
+
+        public int EventId { get; set; }
+        public virtual Event Event { get; set; }
+
+        // Payload data
+        public string Note { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
     #endregion
 
     #region DbContext & Configuration
@@ -83,6 +128,8 @@ namespace G_Net_40_EF02
         public DbSet<OrganizerProfile> OrganizerProfiles { get; set; }
         public DbSet<Attendee> Attendees { get; set; }
         public DbSet<Badge> Badges { get; set; }
+        public DbSet<Event> Events { get; set; } // Added
+        public DbSet<Registration> Registrations { get; set; } // Added
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -93,11 +140,43 @@ namespace G_Net_40_EF02
         {
             base.OnModelCreating(modelBuilder);
 
-            // Fluent API configuration for 1-to-1 relationship between Attendee and Badge
+            // 1. Attendee to Badge (1-to-1)
             modelBuilder.Entity<Attendee>()
                 .HasOne(a => a.Badge)
                 .WithOne(b => b.Attendee)
                 .HasForeignKey<Badge>(b => b.AttendeeId);
+
+            // 2. Q02: 
+            modelBuilder.Entity<Attendee>()
+                .OwnsOne(a => a.Address);
+
+            // 3. Q01: 
+            modelBuilder.Entity<Registration>()
+                .HasKey(r => new { r.AttendeeId, r.EventId });
+
+            modelBuilder.Entity<Registration>()
+                .HasOne(r => r.Attendee)
+                .WithMany(a => a.Registrations)
+                .HasForeignKey(r => r.AttendeeId);
+
+            modelBuilder.Entity<Registration>()
+                .HasOne(r => r.Event)
+                .WithMany(e => e.Registrations)
+                .HasForeignKey(r => r.EventId);
+
+            // 4. Q04: 
+            modelBuilder.Entity<Event>()
+                .HasOne(e => e.ParentEvent)
+                .WithMany(e => e.Sessions)
+                .HasForeignKey(e => e.ParentEventId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // 5. Q03: 
+            modelBuilder.Entity<Event>()
+                .Property<DateTime>("CreatedAt");
+
+            modelBuilder.Entity<Event>()
+                .Property<DateTime>("ModifiedAt");
         }
     }
     #endregion
@@ -107,24 +186,23 @@ namespace G_Net_40_EF02
     {
         static void Main(string[] args)
         {
-
             using (EventHubDbContext context = new EventHubDbContext())
             {
                 Console.WriteLine("\nInitializing EventHub Database...");
 
+                // Drop and recreate to apply new schema cleanly
+                context.Database.EnsureDeleted();
                 bool isCreated = context.Database.EnsureCreated();
 
                 if (isCreated)
                 {
-                    Console.WriteLine("SUCCESS: Database 'EventHubDb' created successfully.");
+                    Console.WriteLine("SUCCESS: Database 'EventHubDb' created successfully with ALL requirements.");
                 }
                 else
                 {
                     Console.WriteLine("INFO: Database already exists.");
                 }
             }
-
-          
         }
     }
     #endregion
